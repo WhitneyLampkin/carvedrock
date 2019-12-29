979b8dea6952fd7a47abd5c88a5ae8f34b6b1b0f
ZionLloyd05 <zionlloyd05@gmail.com>
2019-12-29 02:14:31 +0100

    Project setup

---
+++ CarvedRock.API/GraphQL/CarvedRockSchema.cs
@@ -0,0 +1,16 @@
+﻿using GraphQL.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarvedRock.API.GraphQL
+{
+    public class CarvedRockSchema : Schema
+    {
+        public CarvedRockSchema()
+        {
+            
+        }
+    }
+}
